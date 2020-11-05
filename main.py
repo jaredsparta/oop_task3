b58529a93ff4cd362b8064cc567b9f8db5d6d40b
jared <jtlss14@gmail.com>
2020-11-05 16:34:47 +0000

    pseudo code added

--- main.py
+++ main.py
@@ -27,38 +27,45 @@ for letter in list10:
 #####
 
 class Value:
+    # Every instance keeps track of past words checked
     def __init__(self):
         self.past_words = []
 
-    
+    # Returns the score of a word
     def score(self, word):
         score = 0
         for letter in word.upper():
             score += letters_to_points[letter]
         return score
 
-    
+    # Adds a word to the past words attribute
     def add_word(self, word):
         score = self.score(word)
         self.past_words.append(f"{word} : {score} points")
 
-    
+    # Prints each word in the past words attribute
     def check_past_words(self):
         if not self.past_words:
-            print("You haven't checked any words yet!")
+            print("\nYou haven't checked any words yet!")
         for words in self.past_words:
             print(words)
 
-
+# Initialises the class to use
 game = Value()
 
+# The loop continues until the user decides to stop
 while True:
+    # The user has 3 choices on what to do
     choice = input("""\nWhat would you like to do?
                 1. Find the value of a word
                 2. Check all words that you've typed
                 3. Exit
-                        """)
+                       -> """)
 
+    # If they choose to check the score of a word this happens
+    # The if statement checks if they inputted ONLY letters
+    # Otherwise the word is valid and it adds it to the past words
+    # and displays the score
     if choice == "1":
         word = input("\nWhat word? ")
         if not word.isalpha():
@@ -67,8 +74,10 @@ while True:
         game.add_word(word)
         print(f"{word} has a score of {game.score(word)}")
 
+    # This just lists the words the user has inputted already
     if choice == "2":
         game.check_past_words()
 
+    # This exits the loop
     if choice == "3":
         break
